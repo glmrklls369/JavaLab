8b08c154d29d3c535ad90de792217c319e150c2f
Asia Davis <asia.davis369@gmail.com>
2019-07-23 08:47:05 -0400

    javalabone

---
+++ scripts.js
@@ -0,0 +1,18 @@
+const name = "Asia";
+const age = 23;
+const bday = Dec95;
+const detroitGC = true;
+const lifeEvents = [
+  "I moved to detroit when I was 18",
+  "I graduated from Wayne State",
+  "I am a visual designer",
+  "I enjoy smoking and long walks in the park"
+];
+if (detroitGC === true) {
+    console.log("My name is" + name + "and I am a student at Grand Circus in Detroit, Michigan. I am currently + 'age'+ years old and my birthday is on" + birthday.);
+ 
+for (let i=0, i < 4; i++); 
+console.log (lifeEvents);
+let randomNumber= Math.random() * (1 - 9)
+let counter= 0
+
